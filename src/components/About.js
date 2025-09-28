b5e73b0a773cd55a2d37b3900c86ef9af9616e14
Roman Purgstaller <roman.purgstaller@voestalpine.com>
2025-09-28 19:03:24 +0200

    adjusted about text, add picks texts

--- src/components/About.js
+++ src/components/About.js
@@ -5,29 +5,30 @@ export default function About() {
     <div className="content-container about-container">
       <div className="column-about about-text-col">
         <p>
-          Hey there, I'm Roman. Developer, Tech lead and Product Manager. My first
-          encounter with programming was in high school when I played around with
-          HTML and CSS. I found my passion with tech and coding in University,
-          where I learned about algorithms, touring machines and pointers. I
-          remember when I was reading through the book "C/C++ textbook", when it
-          clicked for me how cool and how much fun programming can be.
+           Hey there, I'm Roman. Developer, Tech lead and Product Manager.
         </p>
         <p>
-          Later, after working a while as a software developer, I got the
+          My journey into programming began high school, where I played around with
+          HTML and CSS. Later, I found my passion with tech and coding in University,
+          where I learned about algorithms, Touring machines and pointers. I still remember the moment it all clicked
+          while reading a C/C++ book — realizing the endless possibilities of programming and how much fun it is.
+        </p>
+        <p>
+          A few years into my career as a software engineer, I got the
           opportunity to lead a small team of developers as a product owner. I had
           no idea what I was getting into. While this would have been a
           challenging transition for any engineer, 2 out of 4 engineers in my team
           left the company before I started taking over the team and the number of
-          bugs was in the low triple digits. With the help of a lot of great
-          people around me, I managed to build a small and effective team that was
-          able to deliver. I especially enjoyed mentoring other developers and
-          working closely with the team to find amazing solutions to cool
-          problems. Even today, I feel that the things I've learned there will
-          benefit me for the rest of my life.
+          bugs was in the triple digits. I was clearly out of my depth! But the team was fantastic, and I was
+          surrounded by amazing people who supported me. Over time, we found our rhythm and started delivering.
+          What stands out most from that period is how much I enjoyed mentoring other developers and
+          working closely with the team to find amazing solutions to cool problems.
+          Even today, I believe that the things I've learned there will benefit me for the rest of my life.
         </p>
         <p>
-          Currently, I work as a tech lead and product manager, where I have the
-          opportunity to combine my technical skills with my passion to lead.
+          Currently, I work as a tech lead and product manager,  combining my technical expertise with my passion for
+          leadership. While my role now involves more strategic thinking, product management and meetings,
+          I still enjoy building the occasional feature, fixing a bug or creating solutions for exciting problems.
         </p>
       </div>
       <div className="column-about about-img-col">
--- src/components/Picks.js
+++ src/components/Picks.js
@@ -1,6 +1,6 @@
-import { ExternalLink } from "../utils/Link";
+import {ExternalLink, ExternalLinkRunningText} from "../utils/Link";
 
-function PickEntry({ labels, name, url }) {
+function PickEntry({ labels, name, url, desc_txt_elem }) {
   let name_elem =
     <div className="picks-name-column">
       {url ? (
@@ -20,9 +20,7 @@ function PickEntry({ labels, name, url }) {
 
   let description =
     <div className="picks-description-column">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      {desc_txt_elem}
     </div>
 
   return (
@@ -34,26 +32,50 @@ function PickEntry({ labels, name, url }) {
 
 
 export default function Picks() {
+  let desc_keepass =
+    <span>
+      KeePass has been my go-to password manager for years. I really appreciate its simplicity and local-first approach.
+      Syncing between devices requires a bit of manual effort, but it works smoothly when combined with Syncthing.
+    </span>
+
+  let desc_vs_code =
+    <span>
+      VS Code has become my editor for... well, (almost) everything! It feels lightweight and intuitive to use,
+      which makes it a great fit for most of my daily tasks. For larger projects, though, I currently prefer PyCharm.
+    </span>
+
+  let desc_pycharm =
+    <span>
+      I love using PyCharm with my current stack — Python, Angular, and PostgreSQL. This is where most of my daily programming happens.
+      While I mostly prefer PyCharm for larger projects, I also started using it for side projects and smaller experiments.
+    </span>
+
+  let desc_bruno =
+    <span>
+      Best API client out there for me!
+    </span>
+
+  let desc_hn = <span></span>
+
   return (
     <div className="content-container">
-    <PickEntry labels={["search-engine", "browser"]} name="DuckDuckGo" url="https://duckduckgo.com/" />
-    <PickEntry labels={["browser"]} name="Firefox" url="https://www.firefox.com/" />
-    <PickEntry labels={["os"]} name="Ubuntu" url="" />
-    <PickEntry labels={["ide", "development"]} name="VS Code" url="https://code.visualstudio.com/" />
-    <PickEntry labels={["ide", "development"]} name="PyCharm" url="https://www.jetbrains.com/pycharm/" />
-    <PickEntry labels={["api-testing", "development"]} name="Bruno" url="https://www.usebruno.com/" />
-    <PickEntry labels={["ide", "database", "development"]} name="pgAdmin" url="https://www.pgadmin.org/" />
-    <PickEntry labels={["ide", "latex", "development"]} name="TeXstudio" url="https://www.texstudio.org/" />
+    <PickEntry labels={["search-engine", "browser"]} name="DuckDuckGo" url="https://duckduckgo.com/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["ide", "development"]} name="VS Code" url="https://code.visualstudio.com/" desc_txt_elem={desc_vs_code} />
+    <PickEntry labels={["ide", "development"]} name="PyCharm" url="https://www.jetbrains.com/pycharm/" desc_txt_elem={desc_pycharm} />
+    <PickEntry labels={["api-testing", "development"]} name="Bruno" url="https://www.usebruno.com/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["ide", "database", "development"]} name="pgAdmin" url="https://www.pgadmin.org/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["ide", "latex", "development"]} name="TeXstudio" url="https://www.texstudio.org/" desc_txt_elem={desc_keepass} />
 
-    <PickEntry labels={["email", "calendar"]} name="Tuta" url="https://mail.tutanota.com/" />
-    <PickEntry labels={["organization"]} name="Obsidian" url="https://obsidian.md/" />
-    <PickEntry labels={["password-manager"]} name="KeePass" url="https://keepass.info/" />
-    <PickEntry labels={["automation"]} name="Autohotkey" url="https://www.autohotkey.com/" />
-    <PickEntry labels={["sync"]} name="Syncthing" url="https://syncthing.net/" />
+    <PickEntry labels={["email", "calendar"]} name="Tuta" url="https://mail.tutanota.com/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["organization"]} name="Obsidian" url="https://obsidian.md/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["password-manager"]} name="KeePass" url="https://keepass.info/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["automation"]} name="Autohotkey" url="https://www.autohotkey.com/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["sync"]} name="Syncthing" url="https://syncthing.net/" desc_txt_elem={desc_keepass} />
 
-    <PickEntry labels={["podcast"]} name="Darknet Diaries" url="https://darknetdiaries.com/" />
-    <PickEntry labels={["podcast"]} name="Hardcore History Series" url="https://www.dancarlin.com/hardcore-history-series/" />
-    <PickEntry labels={["podcast"]} name="NRD Info: Synapsen (german)" url="https://www.ndr.de/nachrichten/info/synapsen-ein-wissenschaftspodcast,podcast2994.html" />
+    <PickEntry labels={["news"]} name="Hacker News" url="https://news.ycombinator.com/" desc_txt_elem={desc_hn} />
+    <PickEntry labels={["podcast"]} name="Darknet Diaries" url="https://darknetdiaries.com/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["podcast"]} name="Hardcore History Series" url="https://www.dancarlin.com/hardcore-history-series/" desc_txt_elem={desc_keepass} />
+    <PickEntry labels={["podcast"]} name="NRD Info: Synapsen (german)" url="https://www.ndr.de/nachrichten/info/synapsen-ein-wissenschaftspodcast,podcast2994.html" desc_txt_elem={desc_keepass} />
     </div>
   );
 }
