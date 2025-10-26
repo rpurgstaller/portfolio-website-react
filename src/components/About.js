7a71c2631233ec8a3c2a471cd1b091a95198e410
Roman Purgstaller <roman.purgstaller@voestalpine.com>
2025-10-26 09:46:32 +0100

    refine practices and about sections

--- src/components/About.js
+++ src/components/About.js
@@ -4,37 +4,24 @@ export default function About() {
   return (
     <div className="content-container about-container">
       <div className="column-about about-text-col">
-        <h2>Hey there, I'm Roman</h2>
-        <p>
-          I'm a Tech lead and Product Manager based in Graz, Austria. After completing my studies at Graz University of Technology,
-          I had the opportunity to work in different fields as a software engineer, architect and product owner.
-          While I am fascinated by the wide range of possibilities in software engineering, I am particularly interested in web development and data analytics.
-        </p>
-        <p>
-          In my free time, I enjoy climbing, hiking, running, or playing a game of chess. Over the past few years,
-          sport climbing has become one of my biggest passions, and I now teach climbing at
-          the <a href="https://www.c-a-c.at/">city adventure center</a> in Graz.
-        </p>
-        <p>
-          Currently, I work as a Tech Lead and Product Manager, combining technical expertise with a strong focus on leadership.
-          While my role involves now more strategic thinking, mentoring and team coordination, I still enjoy building features,
-          fixing bugs, and creating solutions for exciting problems.
-        </p>
-        {/*<h2>Key skills</h2>*/}
-        {/*<div>*/}
-        {/*  <ul>*/}
-        {/*    <li>Building apps and services that drive business value and foster growth</li>*/}
-        {/*    <li>Diving into business processes and complex domains to deliver valuable features</li>*/}
-        {/*    <li>Creating robust and scalable systems with a focus on code that's easy to reuse and evolve</li>*/}
-        {/*    <li>Developing data-driven applications and crafting meaningful visualizations to support informed decision-making</li>*/}
-        {/*    <li>Growing and empowering engineering teams within an agile environment</li>*/}
-        {/*    <li>Fostering a learning culture through honest and compassionate feedback</li>*/}
-        {/*  </ul>*/}
-        {/*</div>*/}
-        {/*<h1>My values</h1>*/}
-        {/*<div>*/}
-
-        {/*</div>*/}
+        <h1>Hey there, I'm Roman</h1>
+        <div>
+          <p>
+            I'm a Tech lead and Product Manager based in Graz, Austria. After completing my studies at Graz University of Technology,
+            I had the opportunity to work in different fields as a software engineer, architect and product owner.
+            While I am fascinated by the wide range of possibilities in software engineering, I am particularly interested in web development and data analytics.
+          </p>
+          <p>
+            In my free time, I enjoy climbing, hiking, running, or playing a game of chess. Over the past few years,
+            sport climbing has become one of my biggest passions, and I now teach climbing at
+            the <a href="https://www.c-a-c.at/">city adventure center</a> in Graz.
+          </p>
+          <p>
+            Currently, I work as a Tech Lead and Product Manager, combining technical expertise with a strong focus on leadership.
+            While my role involves now more strategic thinking, mentoring and team coordination, I still enjoy building features,
+            fixing bugs, and creating solutions for exciting problems.
+          </p>
+        </div>
       </div>
     </div>
   );
